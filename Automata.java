524be8b5f32f6b784b036e917140e906027ee6f9
AbdiVicencio <49078797+AbdiVicencio@users.noreply.github.com>
2019-10-12 23:10:37 -0700

    Update Automata.java

--- Automata.java
+++ Automata.java
@@ -1,4 +1,4 @@
-import java.io.BufferedReader;
+java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -10,6 +10,7 @@ public class Automata {
     public static class transitions{
         int Cstate;
         int Nstate;
+        int Nstate2;
         char symbol;
         transitions Tlist;
     }
@@ -40,92 +41,116 @@ public class Automata {
     }
 
     public static NFA And(NFA NFA1, NFA NFA2){
-
+        //Create a transition class
         transitions tran = new transitions();
         tran.Cstate = NFA1.Estate;
         tran.Nstate = NFA2.Sstate;
         tran.symbol = 'E';
-        //DONT Forget NFA1.Estate = NFA2.Estate;
-        //NFA1.TList.Tlist = tran;
-       //transitions tr = NFA2.TList; 
-        //NFA1.TList.Tlist.Tlist = tr;
+        //Create a temp Transition class to iterate
+        //through the transitions
         transitions tmpTran = new transitions();
-        tmpTran = NFA1.TList;  
+        tmpTran = NFA1.TList;
         while (tmpTran != null) {
-           // System.out.println("Current State: " + tmpTran.Cstate);
-            //System.out.println("Next State: " + tmpTran.Nstate);
-            //System.out.println("Symbol: " + tmpTran.symbol);    
-            //tmpTran = tmpTran.Tlist;
-           if(tmpTran.Tlist == null){
-                break;
-           }
-           tmpTran = tmpTran.Tlist;
-        } 
-        tmpTran.Tlist = tran;
-        transitions tr = NFA2.TList; 
-        tmpTran.Tlist.Tlist = tr;
-        NFA1.Estate = NFA2.Estate;
-
-        tmpTran = NFA1.TList;  
-         System.out.println("Full NFA....................");
-        while (tmpTran != null) {
-           
-            System.out.println("Current State: " + tmpTran.Cstate);
-            System.out.println("Next State: " + tmpTran.Nstate);
-            System.out.println("Symbol: " + tmpTran.symbol); 
             if(tmpTran.Tlist == null){
                 break;
-           }   
+            }
             tmpTran = tmpTran.Tlist;
-         }
-       
-        /*
-        NFA andNFA = new NFA();
-        transitions tran = new transitions();
-        transitions tranTran = new transitions();
-        transitions tranTrantran = new transitions();
+        }
+        //Connect the transitions
+        tmpTran.Tlist = tran;
+        transitions tr = NFA2.TList;
+        tmpTran.Tlist.Tlist = tr;
+        //Connect the transition list to NFA1
+        NFA1.Estate = NFA2.Estate;
 
-        andNFA.Sstate = NFA1.Sstate;
-        andNFA.Estate = NFA2.Estate;
-       
-        tran.Cstate = NFA1.Sstate;
-        tran.Nstate = NFA1.Estate;
-        tran.symbol = NFA1.TList.symbol;
-        andNFA.TList = tran;
 
-        tranTran.Cstate = NFA1.Estate;
-        tranTran.Nstate = NFA2.Sstate;
-        tranTran.symbol = 'E';
-        andNFA.TList.Translist = tranTran;
-
-        tranTrantran.Cstate = NFA2.Sstate;
-        tranTrantran.Nstate = NFA2.Estate;
-        tranTrantran.symbol = NFA2.TList.symbol;
-        andNFA.TList.Translist.Translist = tranTrantran;
-        //transitions tranTran = new transitions();
-       // tranTran.Cstate = NFA1.Estate;
-        //tranTran.Nstate = NFA2.Sstate;
-       // tranTran.symbol = 'E';
-        //NFA1.Estate = NFA2.Estate;
-        //NFA1.TList.Translist = tranTran;
-        
-        
-        
-        //System.out.println(NFA1.Sstate);
-        //System.out.println(NFA1.Estate);
-        //System.out.println(NFA1.TList.symbol);
-        //System.out.println(NFA1.TList.Translist.Cstate);
-        //System.out.println(NFA1.TList.Translist.Nstate);
-        //System.out.println(NFA1.TList.Translist.symbol);
-        */
-       
         return NFA1;
     }
 
-    public static void main(String args[]) throws FileNotFoundException {
-        //int num = 0;
+    public static NFA STAR(NFA oldNFA,int num){
+        NFA starNFA = new NFA();
+        starNFA.Sstate = num;
+        starNFA.Estate = num;
 
-        File infile = new File(args[0]);
+        transitions tr = new transitions();
+        tr.Cstate = starNFA.Sstate;
+        tr.Nstate = oldNFA.Sstate;
+        tr.symbol = 'E';
+        tr.Tlist = oldNFA.TList;
+
+        transitions tempTran = new transitions();
+        tempTran = oldNFA.TList;
+        while (tempTran != null) {
+            if(tempTran.Tlist == null){
+                break;
+            }
+            tempTran = tempTran.Tlist;
+        }
+
+        transitions lastTr = new transitions();
+        lastTr.Cstate = tempTran.Nstate;
+        lastTr.Nstate = starNFA.Sstate;
+        lastTr.symbol = 'E';
+
+        tempTran.Tlist = lastTr;
+        starNFA.TList = tr;
+        return starNFA;
+    }
+
+    public static NFA OR(NFA NFA1,NFA NFA2, int num){
+        NFA orNFA = new NFA();
+        orNFA.Sstate = num;
+        orNFA.Estate = num+1;
+
+        transitions firstTr = new transitions();
+        firstTr.Cstate = orNFA.Sstate;
+        firstTr.symbol = 'E';
+        firstTr.Nstate = NFA1.Sstate;
+        firstTr.Nstate2 = NFA2.Sstate;
+        orNFA.TList = firstTr;
+        transitions NFA1Tran = new transitions();
+        NFA1Tran = NFA1.TList;
+        while (NFA1Tran != null) {
+            if(NFA1Tran.Tlist == null){
+                break;
+            }
+            NFA1Tran = NFA1Tran.Tlist;
+        }
+        transitions NFA2Tran = new transitions();
+        NFA2Tran = NFA2.TList;
+        while (NFA2Tran != null) {
+            if(NFA2Tran.Tlist == null){
+                break;
+            }
+            NFA2Tran = NFA2Tran.Tlist;
+        }
+
+        transitions N1Epsilon = new transitions();
+        N1Epsilon.Cstate = NFA1.Estate;
+        N1Epsilon.Nstate = orNFA.Estate;
+        N1Epsilon.symbol = 'E';
+
+        transitions N2Epsilon = new transitions();
+        N2Epsilon.Cstate = NFA2.Estate;
+        N2Epsilon.Nstate = orNFA.Estate;
+        N2Epsilon.symbol = 'E';
+
+        //orNFA.TList = firstTr;
+        NFA1Tran.Tlist = N1Epsilon;
+        NFA2Tran.Tlist = N2Epsilon;
+        NFA1Tran.Tlist.Tlist = NFA2.TList;
+        orNFA.TList.Tlist = NFA1.TList;
+
+        return orNFA;
+    }
+
+
+
+
+    public static void main(String args[]) throws FileNotFoundException {
+        int count = 0;
+
+        File infile = new File("src/SampleTestfile.txt");
         Scanner input = new Scanner(infile);
         Stack<NFA> stack = new Stack<NFA>();
 
@@ -141,7 +166,7 @@ public class Automata {
             for (Character c:holder) {
 
 
-               // System.out.println(c);
+                // System.out.println(c);
                 if (c.equals('&')) {
                     //nFA2 = pop();
                     //nFA1 = pop();
@@ -150,39 +175,67 @@ public class Automata {
                     NFA NFA2 = stack.pop();
                     NFA NFA1 = stack.pop();
                     stack.push(And(NFA1, NFA2));
-                    
-                    
+
+
+
                 }
                 else if (c.equals('|')) {
-
-
+                    NFA NFA2 = stack.pop();
+                    NFA NFA1 = stack.pop();
+                    stack.push(OR(NFA1, NFA2,num));
+                    num++;
+                    num++;
                 }
                 else if (c.equals('*')) {
 
-                    //nFA = pop();
+
+                    NFA NFA1 = stack.pop();
+                    stack.push(STAR(NFA1, num));
+                    num++;
                     //push(NFA that accepts L(nFA) star);
                     //printf("TEST \n");
                     //printf("Letter:%i",c);
                 }
-                else if( c == 13 || c == 10){
-                    break;
-                }
                 else{
 
                     //nfaMaker(c,num);
-                   // nfaMaker(c,num);
+                    // nfaMaker(c,num);
+
                     stack.push(nfaMaker(c,num));
                     num++;
                     num++;
-                   // NFA test = stack.pop();
+                    // NFA test = stack.pop();
                     //System.out.println(test.Sstate);
                     //System.out.println(test.Estate);
                 }
-              
+
 
             }
 
+            NFA getBack = stack.pop();
+            System.out.println("Count: "+count);
+            count++;
+            if(getBack.TList.Nstate2 != 0){
+                System.out.format("S(q%d,%c)-> q%d,q%d \n",getBack.TList.Cstate, getBack.TList.symbol ,getBack.TList.Nstate,getBack.TList.Nstate2);
+            }
+            else{
+                System.out.format("S(q%d,%c)-> q%d \n",getBack.TList.Cstate,getBack.TList.symbol,getBack.TList.Nstate);
+            }
+            getBack.TList = getBack.TList.Tlist;
+            while(getBack.TList != null){
+                if(getBack.TList.Nstate2 != 0){
+                    System.out.format("(q%d,%c)-> q%d,q%d \n",getBack.TList.Cstate,getBack.TList.symbol,getBack.TList.Nstate,getBack.TList.Nstate2);
+                }
+                else if(getBack.TList.Nstate == getBack.Estate){
+                    System.out.format("F(q%d,%c)-> q%d \n",getBack.TList.Cstate,getBack.TList.symbol,getBack.TList.Nstate);
+                }
+                else{
+                    System.out.format("(q%d,%c)-> q%d \n",getBack.TList.Cstate,getBack.TList.symbol,getBack.TList.Nstate);
+                }
 
+                getBack.TList = getBack.TList.Tlist;
+            }
+            System.out.println("_____________________________________________________________________________________________");
         }
 
 
